242a80edb9204bfb28f4529d634533bf0b7e8b2e
Keith Salisbury <keith.salisbury@alliants.com>
2015-07-29 23:23:18 +0100

    stop the system user noise

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -11,7 +11,7 @@ defmodule ElixirLetters.RoomChannel do
   """
   def join("rooms:lobby", message, socket) do
     Process.flag(:trap_exit, true)
-    :timer.send_interval(5000, :ping)
+    #:timer.send_interval(5000, :ping)
     send(self, {:after_join, message})
 
     {:ok, socket}
