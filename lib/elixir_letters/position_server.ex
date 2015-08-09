46527c3630105460268037e7584a14e183565898
Keith Salisbury <keith.salisbury@alliants.com>
2015-08-09 17:58:54 +0100

    better code

--- lib/elixir_letters/position_server.ex
+++ lib/elixir_letters/position_server.ex
@@ -3,7 +3,15 @@ defmodule ElixirLetters.PositionServer do
   require Logger
 
   def start_link do
-    GenServer.start_link(__MODULE__, %{positions: %{},users: %{},user_count: 0}, name: __MODULE__)
+    GenServer.start_link(
+      __MODULE__,
+      %{
+        positions: %{},
+        users: %{},
+        user_count: 0
+        },
+      name: __MODULE__
+    )
   end
 
   def update_position(user_id, position) do
@@ -29,33 +37,27 @@ defmodule ElixirLetters.PositionServer do
     GenServer.call(__MODULE__, :get_user_count)
   end
 
-  def handle_call({:update_position, user_id, position}, _caller, state) do
+  def handle_call(
+      {:update_position, user_id, position},
+      _caller,
+      %{positions: positions} = state) do
     %{"id" => letter_id, "left" => left, "top" => top} = position
-    state = Map.merge(state,
-      %{
-        positions: Map.put(state.positions, letter_id, position)
-      })
-    {:reply, :ok, state}
+    positions = Map.put(positions, letter_id, position)
+    {:reply, :ok, %{state | positions: positions}}
   end
 
   def handle_call(:get_positions, _caller, state) do
     {:reply, state.positions, state}
   end
 
-  def handle_call({:add_user, user_id, user}, _caller, state) do
-    state = Map.merge(state,
-      %{
-        users: Map.put(state.users, user_id, user),
-      })
-    {:reply, :ok, state}
+  def handle_call({:add_user, user_id, user}, _caller, %{users: users} = state) do
+    users = Map.put(users, user_id, user)
+    {:reply, :ok, %{state | users: users}}
   end
 
-  def handle_call({:remove_user, user_id}, _caller, state) do
-    state = Map.merge(state,
-      %{
-        users: Map.delete(state.users, user_id),
-      })
-    {:reply, :ok, state}
+  def handle_call({:remove_user, user_id}, _caller, %{users: users} = state) do
+    users = Map.delete(users, user_id)
+    {:reply, :ok, %{state | users: users}}
   end
 
   def handle_call(:get_user_count, _caller, state) do
