3e6c80f3aaf1be016c347e6aa433af94b41267f5
Keith Salisbury <keith.salisbury@alliants.com>
2015-10-14 22:50:29 +0100

    wip

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -47,6 +47,8 @@ class App {
 
       $("#letters-container").show()
 
+      this.setupPixi()
+
       $("#content").keydown(function (event){
         //console.log("You pressed the key: ", String.fromCharCode(event.keyCode))
       })
@@ -149,6 +151,87 @@ class App {
         .css('left', pos.left)
     }
   }
+
+  static setupPixi() {
+    const renderer = PIXI.autoDetectRenderer(800, 600);
+
+    renderer.resize(window.innerWidth, window.innerHeight);
+
+    document.body.appendChild(renderer.view);
+
+    // create the root of the scene graph
+    var stage = new PIXI.Container();
+
+    // add a shiny background...
+    var background = PIXI.Sprite.fromImage('/images/textDemoBG.jpg');
+    stage.addChild(background);
+
+    // create some white text using the Snippet webfont
+    var textSample = new PIXI.Text('Pixi.js can has\n multiline text!', { font: '35px Snippet', fill: 'white', align: 'left' });
+    textSample.position.set(20);
+    stage.addChild(textSample);
+
+    requestAnimationFrame(animate);
+    function animate() {
+      renderer.render(stage);
+      requestAnimationFrame(animate);
+    }
+
+  }
+
+
+  // // Load them google fonts before starting...!
+  // window.WebFontConfig = {
+  //     google: {
+  //         families: ['Snippet', 'Arvo:700italic', 'Podkova:700']
+  //     },
+  //
+  //     active: function() {
+  //         // do something
+  //         init();
+  //     }
+  // };
+
+  // function init()
+  // {
+      // PIXI.loader
+      //     .add('desyrel', '_assets/desyrel.xml')
+      //     .load(onAssetsLoaded);
+      //
+      // function onAssetsLoaded()
+      // {
+      //     var bitmapFontText = new PIXI.extras.BitmapText('bitmap fonts are\n now supported!', { font: '35px Desyrel', align: 'right' });
+      //
+      //     bitmapFontText.position.x = 600 - bitmapFontText.textWidth;
+      //     bitmapFontText.position.y = 20;
+      //
+      //     stage.addChild(bitmapFontText);
+      // }
+
+      //
+
+      // create a text object with a nice stroke
+      // var spinningText = new PIXI.Text('I\'m fun!', { font: 'bold 60px Arial', fill: '#cc00ff', align: 'center', stroke: '#FFFFFF', strokeThickness: 6 });
+
+      // setting the anchor point to 0.5 will center align the text... great for spinning!
+      // spinningText.anchor.set(0.5);
+      // spinningText.position.x = 310;
+      // spinningText.position.y = 200;
+      //
+      // // create a text object that will be updated...
+      // var countingText = new PIXI.Text('COUNT 4EVAR: 0', { font: 'bold italic 60px Arvo', fill: '#3e1707', align: 'center', stroke: '#a4410e', strokeThickness: 7 });
+
+      // countingText.position.x = 310;
+      // countingText.position.y = 320;
+      // countingText.anchor.x = 0.5;
+
+      // stage.addChild(spinningText);
+      // stage.addChild(countingText);
+
+      // var count = 0;
+      //
+  // }
+
 
 }
 
